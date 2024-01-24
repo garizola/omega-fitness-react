cc01dfb7811d9d4f5d8373c7db6b92f739acceb7
garizola <99741841+garizola@users.noreply.github.com>
2024-01-24 17:52:28 -0500

    updated ui

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,18 @@
-import React, {useEffect, useState} from 'react'
-import { Box, Stack, Typography } from '@mui/material'
-import Logo from '../assets/images/gymLogo-logo-white.png'
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/gymLogo-logo-white.png";
 
 const Footer = () => {
   return (
-    <Box
-      mt='80px'
-      bgcolor='#FB2576'
-      
-    >
-      <Stack
-        gap='40px'
-        alignItems='center'
-        px='40px'
-        pt='26px'
-      >
-        <img  src={Logo} alt='logo' width='64px' height='64'/>
-        <Typography
-          variant='h6'
-          pb='40px'
-        >
-
+    <Box mt="80px" bgcolor="#FB2576">
+      <Stack gap="40px" alignItems="center" px="40px" pt="16px">
+        <img src={Logo} alt="logo" width="64px" height="64" />
+        <Typography variant="h6" pb="16px">
           Omega Fitness | Giovanni Arizola
         </Typography>
-        
-
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -1,106 +1,103 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Button, Stack, TextField, Typography} from '@mui/material';
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 // import { handleBreakpoints } from '@mui/system';
-import { exerciseOptions, fetchData } from '../utils/fetchData';
-import HorizontalScrollbar from './HorizontalScrollbar';
+import { exerciseOptions, fetchData } from "../utils/fetchData";
+import HorizontalScrollbar from "./HorizontalScrollbar";
 
+const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
+  const [search, setSearch] = useState("");
 
-
-const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
-
-  const [search, setSearch] = useState('');
-  
   const [bodyParts, setBodyParts] = useState([]);
 
   useEffect(() => {
     const fetchExercisesData = async () => {
-      const bodyPartsData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions);
+      const bodyPartsData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+        exerciseOptions
+      );
 
-      setBodyParts(['all', ...bodyPartsData]);
-    }
+      setBodyParts(["all", ...bodyPartsData]);
+    };
     fetchExercisesData();
-  }, [])
-  
+  }, []);
 
   const handleSearch = async () => {
-    if(search) {
-      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
+        exerciseOptions
+      );
 
       // console.log(exercisesData);
 
       const searchedExercises = exercisesData.filter(
-        (item) => item.name.toLowerCase().includes(search)
-               || item.target.toLowerCase().includes(search)
-               || item.equipment.toLowerCase().includes(search)
-               || item.bodyPart.toLowerCase().includes(search),
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
       );
 
-      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
-      setSearch('');
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+      setSearch("");
       setExercises(searchedExercises);
     }
-  }
-
+  };
 
   return (
-    <Stack 
-      alignItems='center'
-      mt='40px'
-      justifyContent='center'
-      p='20px'
-    >
+    <Stack alignItems="center" mt="40px" justifyContent="center" p="20px">
       <Typography
         fontWeight={700}
         sx={{
-          fontSize: {lg: '44px', xs: '30px'}
+          fontSize: { lg: "44px", xs: "30px" },
         }}
-        mb='50px'
-        textAlign='center'
+        mb="50px"
+        textAlign="center"
       >
         Discover Exercises
       </Typography>
-      <Box
-        position='relative'
-        mb='72px'
-        
-      >
+      <Box position="relative" mb="72px">
         <TextField
           sx={{
-            input: {fontWeight: '700', border: 'none', borderRadius: '0px', color: '#150050'},
-            width: {lg: '890px', xs: '350px'},
-            backgroundColor: '#fff',
+            input: {
+              fontWeight: "700",
+              border: "none",
+              borderRadius: "0px",
+              color: "#150050",
+            },
+            width: { lg: "890px", xs: "350px" },
+            backgroundColor: "#fff",
             opacity: 0.8,
-            borderRadius: '10px',
-            height: '50px',
-           
+            borderRadius: "10px",
+            height: "50px",
           }}
-          
-          
           value={search}
           onChange={(e) => {
-            setSearch(e.target.value.toLowerCase())
+            setSearch(e.target.value.toLowerCase());
           }}
-          placeholder='Search Exercises...'
-          type='text'
+          placeholder="Search Exercises..."
+          type="text"
         />
-        <Button className='search-btn'
-          variant='contained' color='secondary'
+        <Button
+          className="search-btn"
+          variant="contained"
+          color="secondary"
           sx={{
-          backgroundColor: '#FB2576',
-          padding: '10px 20px',
-          color: '#150050',
-          // textTransform: 'none',
-          width: {lg: '130px', xs: '80px'},
-          fontSize: {lg: '16px', xs: '12px'},
-          height: '50px',
-          // marginLeft: '20px'
+            backgroundColor: "#FB2576",
+            padding: "10px 20px",
+            color: "#150050",
+            // textTransform: 'none',
+            width: { lg: "130px", xs: "80px" },
+            fontSize: { lg: "16px", xs: "12px" },
+            height: "50px",
+            // marginLeft: '20px'
           }}
           onClick={handleSearch}
-
-        >SEARCH</Button>
-
+        >
+          SEARCH
+        </Button>
       </Box>
-        <Box
+      {/* <Box
           sx={{
             position: 'relative',
             width: '100%',
@@ -108,10 +105,9 @@ const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
           }}
         >
           <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setBodyPart={setBodyPart} isBodyPart />
-        </Box>
-
+        </Box> */}
     </Stack>
-  )
-}
+  );
+};
 
-export default SearchExercises
+export default SearchExercises;
